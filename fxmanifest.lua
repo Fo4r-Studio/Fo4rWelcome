a210e692abe1659e62c3b4cba0df3327902e05be
TheLion051 <esteban051401@hotmail.com>
2023-09-17 22:41:37 -0600

    Base

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Welcome Script'
+author 'Fo4r Studios'
+
+lua54 'yes'
+
+client_scripts {
+  'client.lua',
+}
+
+server_scripts {
+  'server.lua',
+}
+
+shared_scripts {
+  'cfg.lua'
+}
+
+ui_page {
+  'ui/index.html',
+}
+files {
+  'ui/*.css',
+  'ui/*.html',
+  'ui/*.js'
+}
