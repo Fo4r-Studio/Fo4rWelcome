407f291b8b53a8ec98a5c488a6fbe5ec345f85b0
TheLion051 <esteban051401@hotmail.com>
2023-09-20 04:48:21 -0600

    Version 1.0.1

--- cfg.lua
+++ cfg.lua
@@ -8,46 +8,45 @@ Config.Webhook = 'https://discord.com/api/webhooks/1153646268166451301/TjQFOqNq7
 Config.Labels = {
     ServerName = "Fo4r",
     Press = 'Press ~g~[E]~w~ to open menu',
-    WelcomeMessage = 'Welcome to Fo4r',
     Webhook1 = 'Has claimed the welcome package',
     Webhook2 = 'Has redeemed the code: ',
 }
 
-Config.Coords = {x = -1036.0082, y = -2734.3013, z = 20.1693, h = 138.3146}
-
 Config.CarCoordsSpawn = {x = -1029.14, y = -2732.87, z = 20.075, h = 240.1851}
 
-Config.Ped = { --else change to false
+Config.Coords = {x = -1036.0082, y = -2734.3013, z = 20.1693, h = 138.3146}
+
+Config.Ped = { --if not change it to false
     model = "cs_milton",
     animation = {anim = "mini@strip_club@idles@bouncer@base", dict = "base"},
 }
 
 Config.FirstReward = {
-    items = { --else change to false
+    items = { --if not change it to false
         {item = "water", count = 2},
         {item = "bread", count = 2},
         {item = "phone", count = 1},
     },
-    money = { --else change to false
+    money = { --if not change it to false
         {account = "money", count = 5000},
         {account = "bank", count = 1000},
     },
-    Carname = "akuma", --else change to false
+    Carname = "akuma", --if not change it to false
 }
 
 Config.CreatorRewards = {
     ["lion"] = { -- Creator code
-        items = { --else change to false
+        items = { --if not change it to false
             {item = "water", count = 2},
             {item = "bread", count = 2},
             {item = "phone", count = 1},
         },
-        money = { --else change to false
+        money = { --if not change it to false
             {account = "money", count = 5000},
             {account = "bank", count = 1000},
             -- {account = "coins", count = 100},
         },
-        Carname = "futo", --else change to false
+        Carname = "futo", --if not change it to false
     }
 }
 
